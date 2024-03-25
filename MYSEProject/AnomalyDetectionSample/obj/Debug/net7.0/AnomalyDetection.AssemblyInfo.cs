82dbea595a6751a8088afa15f368d716afaca449
HASIBUZZAMAN <hasibniloy14@gmail.com>
2024-03-25 10:22:03 +0100

    changes

--- MYSEProject/AnomalyDetectionSample/obj/Debug/net7.0/AnomalyDetection.AssemblyInfo.cs
+++ MYSEProject/AnomalyDetectionSample/obj/Debug/net7.0/AnomalyDetection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnomalyDetection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddd51b16208c8f8cebf6eb502583abe2112328be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnomalyDetection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnomalyDetection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
